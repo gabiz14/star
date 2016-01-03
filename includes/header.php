9a0c4fb7f0272a784098e5e94985426e7cbfb916
Matty <mattytv11@gmail.com>
2016-01-03 21:57:07 +0000

    scrolling

--- includes/header.php
+++ includes/header.php
@@ -3,5 +3,10 @@
 		<h1>Star Wars</h1>
 		<h2>Unoffical Fact Site</h2>
 	</div>
-	<div class="scrollDown"><h2>v</h2></div>
+	<div class="scrollDown">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada pulvinar arcu at commodo. Cras placerat ex tortor, quis varius arcu lobortis vitae. Suspendisse tempor pharetra commodo. Curabitur dictum efficitur orci vel consectetur. Fusce volutpat scelerisque libero, et fermentum orci sollicitudin ut. Fusce orci sem, interdum eget leo nec, pulvinar interdum ipsum. Duis hendrerit sit amet diam sit amet ullamcorper. Donec ut lacinia diam. Morbi non mollis ex, vitae dignissim lorem. Mauris dapibus nulla eu lectus tincidunt, eu imperdiet lorem blandit.</p>
+
+		<p>Suspendisse potenti. Praesent lectus lacus, elementum eu urna id, cursus faucibus neque. Donec vulputate lacus eu bibendum accumsan. Nulla vel turpis eu ligula tincidunt auctor. Suspendisse a blandit est.</p>
+		<img src="../assets/media/arrow-down.png"></img>
+	</div>
 </div>
